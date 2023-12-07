b991ff5e331fce5df9da35bda18ec4eec308ce51
OSJ2623 <osong23@hanmail.net>
2023-12-07 17:48:06 +0900

    podcast 음원 재생/일시정지 구현

--- crews/components/PodCastScreen/PlayerBottom.js
+++ crews/components/PodCastScreen/PlayerBottom.js
@@ -1,17 +1,70 @@
 import React from 'react';
-import { View, Text, } from 'react-native';
+import { View, Text } from 'react-native';
 import { useState, useEffect } from 'react';
 import { Feather, MaterialIcons, FontAwesome5 } from '@expo/vector-icons'; 
 import Slider from '@react-native-community/slider';
 import { PlayerScreenStyles } from '../../styles/PodCastScreen/styles';
 import colors from '../../styles/colors';
 import { TouchableOpacity } from 'react-native';
-//import Sound from 'react-native-sound';
+import { Audio } from 'expo-av';
 
 
 
 const Bottom = (props) => {
+
+    const [sound, setSound] = useState();
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [podcastId, setPodcastId] = useState();
+    const [duration, setDuration] = useState(props.duration);
+    const [position, setPosition] = useState('0');
+    const [slider, setSlider] = useState();
+
+    const musicStatus = async status => {
+        if (!status.isLoaded) {
+        if (status.error) {
+            console.log(`Error on expo AV: ${status.error}`);
+        }
+        } else {
+        setDuration(status.durationMillis);
+        setPosition(status.positionMillis);
+        }
+    };
     
+        async function playSound() {
+            console.log('Loading Sound');
+            const { sound } = await Audio.Sound.createAsync({uri: props.uri},{ shouldPlay: true }, musicStatus);
+            setSound(sound);
+        }
+
+        const playController = async () => {
+            if (!sound) {
+            return;
+            }
+            isPlaying ? await sound.pauseAsync() : await sound.playAsync();
+        };
+
+        useEffect(() => {
+            //setPodcastId(props.id);
+            if (sound) sound.unloadAsync(); // unload
+            playSound();
+        }, []);
+
+        useEffect(() => {
+            playController();
+        }, [isPlaying]);
+
+        // useEffect(() => {
+        //     if (sound) sound.unloadAsync(); // unload
+        //     playSound();
+        // }, [podcastId]);
+
+        const getProgress = () => {
+            if (sound === null || duration === null || position === null) return 0;
+            let slider = Math.floor((position / duration) * 100);
+            //dispatch(setSlider(slider, position, duration));
+            setSlider(slider, position, duration)
+            return slider;
+        };
 
     return (
         <View style = {[PlayerScreenStyles.view, {flex: 1, backgroundColor: '#ffffff'}]}>
@@ -33,7 +86,9 @@ const Bottom = (props) => {
             <Slider
                 style={{width: 350, height: 30}}
                 minimumValue={0}
-                maximumValue={1}
+                maximumValue={duration}
+                value={slider}
+                onValueChange = {()=>{getProgress()}}
                 minimumTrackTintColor={colors.primary}
                 maximumTrackTintColor='#a0a0a0'
                 thumbTintColor={colors.primary}
@@ -41,8 +96,9 @@ const Bottom = (props) => {
             </View>
             <View style = {[PlayerScreenStyles.playBtn]}>
                 <View><MaterialIcons name="skip-previous" size={50} color="black" /></View>
-                <TouchableOpacity onPress={()=>{/*toggle*/}}>
-                    <MaterialIcons name="pause" size={50} color="black" />
+                <TouchableOpacity onPress={()=>{setIsPlaying(!isPlaying)}}>
+                    {isPlaying? <MaterialIcons name="play-arrow" size={50} color="black" />:
+                    <MaterialIcons name="pause" size={50} color="black" />}
                 </TouchableOpacity>
                 <View><MaterialIcons name="skip-next" size={50} color="black" /></View>
             </View>
